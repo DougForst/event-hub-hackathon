bbccdd4804458e5d2069a0469a163baadf7e25ca
Doug Forst <doug.forst@flosports.tv>
2021-07-28 18:18:09 -0500

    overview stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,37 @@
-import { NewsComponent } from './overview/news/news.component';
 import { NgModule } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
-import  {MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BracketComponent } from './bracket/bracket.component';
-import { MainComponent } from './main/main.component';
-import { NewsComponent as OverviewNewsComponent } from './overview/news/news.component';
 import { AthletesComponent } from './athletes/athletes.component';
+import { BracketComponent } from './bracket/bracket.component';
 import { HeaderComponent } from './header/header.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MainComponent } from './main/main.component';
+import { NewsComponent } from './news/news.component';
+import { NewsOverviewComponent } from './overview/news/news-overview.component';
 import { OverviewComponent } from './overview/overview.component';
 import { TeamScoresComponent } from './overview/team-scores/team-scores.component';
 import { TilesComponent } from './overview/tiles/tiles.component';
+import { VideosComponent } from './overview/videos/videos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BracketComponent,
     NewsComponent,
-    OverviewNewsComponent,
+    NewsOverviewComponent,
     MainComponent,
     AthletesComponent,
     HeaderComponent,
     OverviewComponent,
     TeamScoresComponent,
     TilesComponent,
+    VideosComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/overview/videos/videos.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+interface IVideo {
+    name: string;
+    url: string;
+}
+
+@Component({
+  selector: 'app-overview-videos',
+  templateUrl: './videos.component.html',
+  styleUrls: ['./videos.component.scss']
+})
+export class VideosComponent implements OnInit {
+    public videos: IVideo[] = [
+        {
+            url: '/assets/videos/174.jpeg',
+            name: '174 lbs Semifinal - Bo Nickal, Penn State vs Nate Jackson, Indiana',
+        },
+        {
+            url: '/assets/videos/125.jpeg',
+            name: '125 lbs Final - Nico Megaludis, Penn State vs Thomas Gilman, Iowa',
+        },
+        {
+            url: '/assets/videos/157.jpeg',
+            name: '157 lbs Consolation - Nick Brascetta, VT vs Chad Walsh, RID',
+        },
+        {
+            url: '/assets/videos/165.jpeg',
+            name: '165 lbs Consolation - Steven Rodrigues, ILL vs Daniel Lewis, MIZZ',
+        },
+        {
+            url: '/assets/videos/184.jpeg',
+            name: '184 lbs Consolation - Matt Miller, NAVY vs Pete Renda, NCST',
+        },
+    ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
